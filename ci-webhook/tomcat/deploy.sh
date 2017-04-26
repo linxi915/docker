28a481b67c1e2413b7c93f74ab1e6398c45e6d41
KennyLee <kennylee26@gmail.com>
2017-04-26 17:40:27 +0800

    修改supervisord的默认配置，发现一些情况下conf.d下的supervisord.conf文件没生效

--- ci-webhook/tomcat/deploy.sh
+++ ci-webhook/tomcat/deploy.sh
@@ -3,6 +3,7 @@
 PROJECT_NAME=gkxt
 REPO_BRANCH=feature_testci
 CUSTOM_JAVA_OPTS=
+BUILD_COMMAND="./gradlew --no-daemon clean dist -x test"
 
 if [ -n "${JAVA_OPTS}" ]; then
     CUSTOM_JAVA_OPTS="$JAVA_OPTS"
@@ -18,8 +19,9 @@ fi
 ${CATALINA_HOME}/bin/catalina.sh stop
 sleep 3s
 cd /tmp/$PROJECT_NAME
+git checkout $REPO_BRANCH
 git fetch origin $REPO_BRANCH
 git reset --hard origin/$REPO_BRANCH
-./gradlew --no-daemon clean dist -x test
+$BUILD_COMMAND
 /bin/bash -c "rm -rf ${TOMCAT_WEBAPPS_PATH}* && unzip /tmp/$PROJECT_NAME/build/dist/*.zip -d $TOMCAT_WEBAPPS_PATH && ${CATALINA_HOME}/bin/catalina.sh run"
 
