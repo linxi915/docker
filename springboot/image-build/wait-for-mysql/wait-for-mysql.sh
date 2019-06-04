c55031f95cbf19d9f5e4eaf033a80c8aad551d07
KennyLee <kennylee26@gmail.com>
2019-06-04 16:43:40 +0800

    修正mysql和springboot-wait-for-mysql镜像问题

--- springboot/image-build/wait-for-mysql/wait-for-mysql.sh
+++ springboot/image-build/wait-for-mysql/wait-for-mysql.sh
@@ -7,20 +7,39 @@ host=localhost
 username=root
 password=
 
-cmd="$@"
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+key="$1"
 
-if [[ -n "${MYSQL_PORT}" ]]; then
-    port="${MYSQL_PORT}"
-fi
-if [[ -n "${MYSQL_HOST}" ]]; then
-    host="${MYSQL_HOST}"
-fi
-if [[ -n "${MYSQL_USER}" ]]; then
-    username="${MYSQL_USER}"
-fi
-if [[ -n "${MYSQL_PASSWORD}" ]]; then
-    password="${MYSQL_PASSWORD}"
-fi
+case ${key} in
+    -p|--password)
+    password="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -u|--user)
+    username="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -H|--host)
+    host="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -P|--port)
+    port="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 password_arg=
 if [[ -n ${password} ]]; then
@@ -31,7 +50,7 @@ counter=1
 waiting_dot='.'
 while ! mysql --protocol TCP -h"${host}" -P"${port}" -u"${username}" ${password_arg} -e "show databases;" > /dev/null 2>&1; do
     sleep 1
-    ehco "${waiting_dot}"
+    echo "${waiting_dot}"
     waiting_dot+='.'
     counter=`expr ${counter} + 1`
     if [[ ${counter} -gt ${max_counter} ]]; then
@@ -39,7 +58,4 @@ while ! mysql --protocol TCP -h"${host}" -P"${port}" -u"${username}" ${password_
         exit 1
     fi;
 done
-
-echo "MySQL is up - executing command"
-exec ${cmd}
 
